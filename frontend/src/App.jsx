13cafac5fc30d59ddf9c43aa91c53fb4987e419a
Vishvesh Soni <vishveshsoni2003@gmail.com>
2025-10-01 18:49:16 +0530

    Added the necessary assests and created a Navbar for the website

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Collection from './pages/collection'
+import About from './pages/About'
+import Cart from './pages/cart'
+import Contact from './pages/contact'
+import Home from './pages/home'
+import Login from './pages/login'
+import Orders from './pages/orders'
+import PlaceOrder from './pages/placeOrder'
+import Product from './pages/product'
+import Navbar from './components/Navbar'
+
+
+
+export default function App() {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
+ <Routes>
+  <Route path='/'  element={<Home/>} />
+  <Route path='/collection' element={<Collection/>}/>
+  <Route path='/about'  element={<About/>} />
+  <Route path='/cart' element={<Cart/>}/>
+  <Route path='/contact'  element={<Contact/>} />
+  <Route path='/login' element={<Login/>}/>
+  <Route path='/orders'  element={<Orders/>} />
+  <Route path='/placeOrder' element={<PlaceOrder/>}/>
+  <Route path='/product/:productId'  element={<Product/>} />
+ </Routes>
+    </div>
+  );
+}
