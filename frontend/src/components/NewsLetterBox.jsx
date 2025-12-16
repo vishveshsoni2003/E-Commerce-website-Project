d1674d06c50224c063b6bc1ee3c6ced4b7ee46e2
Vishvesh Soni <vishveshsoni2003@gmail.com>
2025-12-17 00:20:56 +0530

    Designed the About Us and Contact Us page and completed the UI part of website, also setup the backend of the project and created a basic server

--- frontend/src/components/NewsLetterBox.jsx
+++ frontend/src/components/NewsLetterBox.jsx
@@ -12,7 +12,7 @@ const NewsLetterBox = () => {
      <p className='text-gray-400 mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis eum cum cumque nesciunt nobis suscipit assumenda similique pariatur ipsam temporibus, molestiae quas tempore voluptas reiciendis commodi eaque, perspiciatis inventore.</p>
     <form onSubmit = {onSubmitHandler}className = 'w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
         <input className = 'w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required/>
-        <button type='submit' className='bg-black text-white text-xs px-10 py-4'>SUBSCRIBE</button>
+        <button type='submit' className='bg-black text-white text-xs px-10 py-4 cursor-pointer'>SUBSCRIBE</button>
     </form>
     </div>
   )
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsLetterBox from '../components/NewsLetterBox'
 
 export default function About() {
   return (
-    <div></div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+
+        <div className='my-10 flex flex-col md:flex-row gap-16'>
+          <img  className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+          <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aliquid a eum ipsam magni perferendis dolorem, hic fuga quam itaque voluptatem accusantium. Dolor vero vel tempore atque earum pariatur sequi?</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates quibusdam voluptatum, assumenda vitae sint error veniam distinctio autem quas magni dolores eaque non delectus nisi quidem tenetur, aliquid beatae eos.</p>
+            <b className='text-gray-800'>Our Mission</b>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus mollitia odit corrupti, ab beatae laborum sint excepturi ipsa perspiciatis debitis fugit magnam quas error illum id nostrum ex autem omnis!</p>
+          </div>
+        </div>
+        <div className='text-2xl py-4'>
+          <Title text1={'WHY'} text2={'CHOOSE US'} />
+        </div>
+
+        <div className='flex flex-col md:flex-row text-sm mb-20'>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Quality Assurance:</b>
+            <p className='text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Convenience:</b>
+            <p className='text-gray-600'>With our user-friendly interface and hasle-free ordering process, shopping has never been easier.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Exceptional Customer Service:</b>
+            <p className='text-gray-600'>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority</p>
+          </div>
+        </div>
+
+        <NewsLetterBox/>
+
+    </div>
   )
 }
